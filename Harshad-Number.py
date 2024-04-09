7ca1533434bdd6c8b0c0fe75df8620cdf578f35b
oondels <hendriusfelix@gmail.com>
2024-04-08 23:13:40 -0300

    solution

--- Harshad-Number.py
+++ Harshad-Number.py
@@ -7,5 +7,3 @@ def HasghedNumber(x: int) -> int:
         sum += x%10 # Extrai o resto da divisão -> 121/10, resta => 1
         x = x//10 # Pega o valor da divisão, ignorando o resto -> 121/10 => 12
     return sum
-
-print(HasghedNumber(121))
